8cc5e94984511cac11f6cd027a6e605cbcb3834c
Andrew T <helios@Sigil.local>
2023-12-21 15:25:44 +0200

    fixed tabs

--- contracts/Turbo.sol
+++ contracts/Turbo.sol
@@ -5,12 +5,12 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Turbo is ERC20{
 
-	constructor(
-		string memory _name,
-		string memory _symbol,
-		address receiver,
-		uint256 volume
-	) ERC20(_name, _symbol){
-		_mint(receiver, volume);
-	}
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        address receiver,
+        uint256 volume
+    ) ERC20(_name, _symbol){
+        _mint(receiver, volume);
+    }
 }
